5baf0a523556d45b9bb0cc518e9e0365dc153cf0
Pavel Kravtsov <pavelkr.msc@gmail.com>
2017-01-11 13:17:40 +0300

    small window for my courses

--- easyLabyrinth/easyLabyrinth/Global.cs
+++ easyLabyrinth/easyLabyrinth/Global.cs
@@ -5,10 +5,10 @@ namespace easyLabyrinth
 {
     static class Global
     {
-        static public int maxX { get; set; } = 48;
-        static public int maxY { get; set; } = 32;
+        static public int maxX { get; set; } = 24;
+        static public int maxY { get; set; } = 18;
 
-        static public double windowSizeModifier { get; set; } = 1900;
+        static public double windowSizeModifier { get; set; } = 1280;
 
         static public Brush backgroundColor { get; set; } = Brushes.Lavender;
         static public Brush lineColor { get; set; } = Brushes.Black;
